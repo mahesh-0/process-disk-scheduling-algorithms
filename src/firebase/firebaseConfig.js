f186207a19d0c36b5981c2ffe256c70363c96b20
mahesh-0 <maheshkumarnalluri7@gmail.com>
2026-03-14 13:40:54 +0530

    commit

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -8,14 +8,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "AIzaSyB19JEFC-xtiVpkcE_cV80pOY2PdCfVhdg",
-  authDomain:
-    import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "process-and-disk-scheduling.firebaseapp.com",
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "process-and-disk-scheduling",
-  storageBucket:
-    import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "process-and-disk-scheduling.firebasestorage.app",
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "600785234107",
-  appId: import.meta.env.VITE_FIREBASE_APP_ID || "1:600785234107:web:0be26a3613e00a97daa19e",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 export const app = initializeApp(firebaseConfig);
